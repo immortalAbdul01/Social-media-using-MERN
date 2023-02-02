924e61914fb165753d20f8d51b6bae59ea2c4468
Immortal <ak2061435@gmail.com>
2023-02-02 09:14:11 +0530

    redux state updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage/login';
+import ProfilePage from './scenes/profilePage/profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />}></Route>
+          <Route path='/home' element={<HomePage />}></Route>
+          <Route path='/profile/:userId' element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
