d7384b90de419c3e5807cd917125880cac1f18db
Immortal <ak2061435@gmail.com>
2023-02-03 21:46:43 +0530

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,33 @@
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
-import HomePage from './scenes/homePage';
-import LoginPage from './scenes/loginPage/login';
-import ProfilePage from './scenes/profilePage/profile';
-import { useMemo } from 'react';
-import { useSelector } from 'react-redux'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import { createTheme } from '@mui/material/styles'
-import { themeSettings } from './theme';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage/index";
+import LoginPage from "./scenes/loginPage/login";
+import ProfilePage from "./scenes/profilePage/profile";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
-  const mode = useSelector((state) =>
-    state.mode
-  )
-  const theme = useMemo(() =>
-    createTheme(themeSettings(mode)), [mode]
-  )
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-
           <Routes>
-            <Route path='/' element={<LoginPage />}></Route>
-            <Route path='/home' element={<HomePage />}></Route>
-            <Route path='/profile/:userId' element={<ProfilePage />}></Route>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
